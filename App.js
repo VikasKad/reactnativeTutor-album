67bd6e770039b2f51c54b9ec4a7ae5c743e93d15
Vikas <neosoft@Neosofts-Mac-mini.local>
2017-11-01 13:30:03 +0530

    added dynamic header text: Passing data from parent to child

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import Header from './src/component/header';
 
 export default class App extends React.Component {
-  render() {
-    return (
-     <Header />
-    );
-  }
+    render() {
+        return (
+            <Header headerText={'Albums !'}/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 })
