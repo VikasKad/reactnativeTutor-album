c5aae132d5666f3bea25da23349a1ec6b31dde2e
Vikas <neosoft@Neosofts-Mac-mini.local>
2017-11-01 15:32:13 +0530

    class based component implementation

--- src/component/AlbumList.js
+++ src/component/AlbumList.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 import {View, Text} from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List !!!</Text>
-        </View>
-    );
+// const AlbumList = () => {
+//     return (
+//         <View>
+//             <Text>Album List !!!</Text>
+//         </View>
+//     );
+// };
+
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    AlbumList !!!
+                </Text>
+            </View>
+        );
+
+    }
 };
 export default AlbumList;
