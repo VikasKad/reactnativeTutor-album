ead140bc1ff8aec373b87c5a9702639e29596124
Vikas <neosoft@Neosofts-Mac-mini.local>
2017-11-01 19:28:07 +0530

    added card section and its other contents

--- src/component/AlbumDetail.js
+++ src/component/AlbumDetail.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View, Text} from 'react-native';
+import Card from './card';
+import CardSection from './cardSection';
 
-const AlbumDetails=(props)=>{
-return(
-    <View>
-        <Text>{props.album.title}</Text>
-    </View>
-);
+const AlbumDetails = (props) => {
+    return (
+        <Card>
+            <CardSection>
+                <View></View>
+                <View>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+
+            </CardSection>
+        </Card>
+    );
 };
+const styles={
+    headerContentStyle:{
+        flexDirection:'column',
+        justifyContent:'space-around'
+    }
+}
 export default AlbumDetails;
