5c77fb67a6998a799dc83e469030c32317935f15
Vikas <neosoft@Neosofts-Mac-mini.local>
2017-11-01 20:28:34 +0530

    Button styling

--- src/component/Button.js
+++ src/component/Button.js
@@ -2,27 +2,32 @@ import React from 'react';
 import {Text,TouchableOpacity} from 'react-native';
 
 const Button = (props) => {
+    const {containerStyle,textStyle}=styles;
     return (
-        <TouchableOpacity>
-            <Text>Click Me !!</Text>
+        <TouchableOpacity style={containerStyle}>
+            <Text style={textStyle}>Click Me !!</Text>
         </TouchableOpacity>
 
     );
 };
 const styles = {
     containerStyle: {
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: {width: 0, height: 2},
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        elevation:1,
+        flex:1,
+        alignSelf:'stretch',
+        backgroundColor:'#fff',
+        borderRadius:5,
+        borderWidth:1,
+        borderColor:'#007aff',
         marginLeft:5,
-        marginRight:5,
-        marginTop:10,
+        marginRight:5
+    },
+    textStyle:{
+        alignSelf:'center',
+        color:'#007aff',
+        fontSize:16,
+        fontWeight:'600',
+        paddingTop:10,
+        paddingBottom:10
     }
 };
 export default Button;
